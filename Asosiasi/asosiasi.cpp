92ec1017e219c51c9f1f04463fc6e5d83b34c42c
muhammadridho-debug <idomuhammad11@gmail.com>
2026-05-11 10:47:21 +0700

    menambahkan implementasi metode tambahDokter dan cetakDokter ke kelas pasien

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -29,3 +29,14 @@ public :
     void tambahPasien(pasien*);
     void cetakPasien();
 };
+
+void pasien :: tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+void pasien :: cetakDokter() {
+    cout << "Daftar pasien dari dokter \"" << this->nama << "\" :\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
