d8fe1f659748247fa4e75459a859c888a0119520
muhammadridho-debug <idomuhammad11@gmail.com>
2026-05-11 11:02:45 +0700

    menambahkan kelas jantung dengan konstruktor dan destruktor

--- Komposisi/jantung.h
+++ Komposisi/jantung.h
@@ -3,3 +3,16 @@
 #include <iostream>
 
 using namespace std;
+
+class jantung {
+public:
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+
+#endif
