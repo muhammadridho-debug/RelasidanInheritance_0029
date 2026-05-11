855b6ebb5c26755590a719a1e93531d05f987da2
muhammadridho-debug <idomuhammad11@gmail.com>
2026-05-11 10:35:21 +0700

    menambahkan kelas Ibu dengan konstruktor dan destruktor

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -1,2 +1,17 @@
 #ifndef IBU_H
 #define IBU_H
+
+class Ibu {
+    public:
+        string nama;
+        vector<string> daftar_anak;
+
+        Ibu(string pNama) : nama(pNama) {
+            cout << "Ibu \"" << nama << "\" ada\n";
+        }
+
+        ~Ibu() {
+            cout << "Ibu \"" << nama << "\" tidak ada\n";
+        }
+
+    };
